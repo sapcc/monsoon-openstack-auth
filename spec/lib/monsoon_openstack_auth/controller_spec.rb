3e40c87634514c6e286566ff35adfa439b907b05
Andreas Pfau <andreas.pfau@sap.com>
2015-04-23 13:23:19 +0200

    ignore empty scope, now it is working :)

--- spec/lib/monsoon_openstack_auth/controller_spec.rb
+++ spec/lib/monsoon_openstack_auth/controller_spec.rb
@@ -7,7 +7,7 @@ describe MonsoonOpenstackAuth::Controller, :type => :controller do
   
   context "skip authentication for an action" do
     controller do # anonymous subclass of ActionController::Base
-      authentication_required region: -> c {c.params[:region_id]}, organization_id: -> c {c.params[:organization_id]}, project_id: -> c {c.params[:project_id]}
+      authentication_required region: -> c {c.params[:region_id]}, organization_id: -> c {c.params[:organization]}, project: -> c {c.params[:project_id]}
       skip_authentication only: [:new]
     
       def index
@@ -32,7 +32,7 @@ describe MonsoonOpenstackAuth::Controller, :type => :controller do
   
   context "skip authentication for all actions" do
     controller do # anonymous subclass of ActionController::Base
-      authentication_required region: -> c {c.params[:region_id]}, organization_id: -> c {c.params[:organization_id]}, project_id: -> c {c.params[:project_id]}
+      authentication_required region: -> c {c.params[:region_id]}, organization: -> c {c.params[:organization_id]}, project: -> c {c.params[:project_id]}
       skip_authentication
     
       def index
@@ -57,20 +57,42 @@ describe MonsoonOpenstackAuth::Controller, :type => :controller do
   
   context "ignore empty scope" do
     controller do # anonymous subclass of ActionController::Base
-      authentication_required region: -> c {c.params[:region_id]}, organization_id: -> c {nil}, project_id: -> c {""}
+      authentication_required region: -> c {c.params[:region_id]}, organization: -> c {nil}, project: -> c {""}
     
       def index
         head :ok
       end
     
-      def new
-        head :ok
-      end
     end
     
     it "authentication should ignore empty organization and project" do
       expect(MonsoonOpenstackAuth::Session).to receive(:check_authentication).with(controller,'europe', organization: nil, project: nil)
       get 'index', region_id: 'europe'
+    end
+      
+  end
+  
+  context "scope not nil" do
+    controller do # anonymous subclass of ActionController::Base
+      authentication_required region: -> c {c.params[:region_id]}, organization: :get_org, project: -> c {c.params[:project_id]}
+    
+      def index
+        head :ok
+      end
+      
+      def get_org
+        params[:organization_id]
+      end
+    end
+    
+    it "authenticate with scope 0-12345 and p-12345" do
+      expect(MonsoonOpenstackAuth::Session).to receive(:check_authentication).with(controller,'europe', organization: "o-12345", project: "p-12345")
+      get 'index', region_id: 'europe', organization_id: 'o-12345', project_id: 'p-12345'
+    end
+    
+    it "authenticate with empty scope" do
+      expect(MonsoonOpenstackAuth::Session).to receive(:check_authentication).with(controller,'europe', organization: nil, project: nil)
+      get 'index', region_id: 'europe', organization_id: '', project_id: ''
     end
       
   end
