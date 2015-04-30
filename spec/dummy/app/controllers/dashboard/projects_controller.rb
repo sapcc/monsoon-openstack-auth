1df1ecf44e7d605048413a5bca9bea94dc3e6189
Torsten Lesmann <torsten.lesmann@sap.com>
2015-04-30 15:19:09 +0200

    Added if_allowed? to controller Changes in Dummy app

--- spec/dummy/app/controllers/dashboard/projects_controller.rb
+++ spec/dummy/app/controllers/dashboard/projects_controller.rb
@@ -1,26 +1,24 @@
 module Dashboard
   class ProjectsController < DashboardController
-    authorization_actions_for :get_project, :only => [:update, :destroy]
-
     def show
       @project = services.identity.user_project(@project_id)
+      if_allowed?("identity:project_show", {id: @project.id}) do
+        @project
+      end
     end
 
     def update
       @project = services.identity.user_project(@project_id)
+      if_allowed?("identity:project_change", {id: @project.id}) do
+        @project
+      end
     end
 
     def destroy
       @project = services.identity.user_project(@project_id)
+      if_allowed?("identity:project_delete", {id: @project.id}) do
+        @project
+      end
     end
   end
-
-  def get_project
-    @project = Project.new
-    @project.id = @project_id
-  end
-end
-
-class Project
-  attr_accessor :id
 end
