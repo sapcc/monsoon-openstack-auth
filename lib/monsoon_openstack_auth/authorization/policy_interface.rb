533461b4b690747dd2c3c885dd4ad62fce46444e
Andreas Pfau <andreas.pfau@sap.com>
2015-07-29 10:20:41 +0200

    fix bug in policy interface

--- lib/monsoon_openstack_auth/authorization/policy_interface.rb
+++ lib/monsoon_openstack_auth/authorization/policy_interface.rb
@@ -4,7 +4,7 @@ module MonsoonOpenstackAuth
       def role_names; raise "Not implemented yet"; end
       def project_domain_id; raise "Not implemented yet"; end
       def domain_id; raise "Not implemented yet"; end
-      def domain_name: raise "Not implemented yet"; end
+      def domain_name; raise "Not implemented yet"; end
       def admin?; raise "Not implemented yet"; end
       def project_id; raise "Not implemented yet"; end
       def id; raise "Not implemented yet"; end
