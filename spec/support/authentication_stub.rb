37979c16a27340996a992d600ef4511e037dd537
Andreas Pfau <andreas.pfau@sap.com>
2016-01-27 13:47:33 +0100

    clone test_token

--- spec/support/authentication_stub.rb
+++ spec/support/authentication_stub.rb
@@ -11,7 +11,7 @@ module AuthenticationStub
   end
   
   def self.test_token
-    @test_token ||= HashWithIndifferentAccess.new(ApiStub.keystone_token.merge("expires_at" => (Time.now+1.hour).to_s))
+    @test_token ||= HashWithIndifferentAccess.new(ApiStub.keystone_token.clone.merge("expires_at" => (Time.now+1.hour).to_s))
   end
   
   def self.domain_id
