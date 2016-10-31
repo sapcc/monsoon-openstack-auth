9b5407a08fbc673e5cc8603993daee32841988fc
Andreas Pfau <andreas.pfau@sap.com>
2016-10-31 11:33:11 +0100

    redirect to main_app root after login instead of :back

--- app/controllers/monsoon_openstack_auth/sessions_controller.rb
+++ app/controllers/monsoon_openstack_auth/sessions_controller.rb
@@ -15,10 +15,10 @@ module MonsoonOpenstackAuth
       redirect_to main_app.root_path, alert: 'Not allowed!' and return unless MonsoonOpenstackAuth.configuration.form_auth_allowed?
       @username = params[:username]
       @password = params[:password]
-      @domain_id = params[:domain_id]
-      @domain_name = params[:domain_name]
+      @domain_id = params[:domain_id].blank? ? nil : params[:domain_id]
+      @domain_name = params[:domain_name].blank? ? nil : params[:domain_name]
       
-      after_login_url = (params[:after_login] || session[:after_login_url] || :back)
+      after_login_url = (params[:after_login] || session[:after_login_url] || main_app.root_url(domain_id: (@domain_id || @domain_name)))
       if MonsoonOpenstackAuth::Authentication::AuthSession.create_from_login_form(self,@username,@password, @domain_id,@domain_name) 
         redirect_to after_login_url
       else
